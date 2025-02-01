80e00b3b55367edf60cd9d341597f8957312ba3d
Aidan Keighron <78337317+aidankeighron@users.noreply.github.com>
2025-02-01 14:12:36 -0500

    updated ui

--- app/admin/input/page.tsx
+++ app/admin/input/page.tsx
@@ -2,7 +2,7 @@ import AddCSVButton from "@/components/addCSVButton";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen p-8 pb-20 gap-16">
+    <div className="flex flex-col min-h-screen p-8 pb-20 gap-16">
         <AddCSVButton name="In / Out" />    
         <AddCSVButton name="Flow" />    
     </div>
--- components/addCSVButton.tsx
+++ components/addCSVButton.tsx
@@ -1,23 +1,41 @@
 'use client'
 import { Button } from "@/components/ui/button";
 import { addLocation } from "@/lib/mongo";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 type AddCSVButtonProps = {
-    name: string,
+    title: string,
 }
 
-export default function AddCSVButton({name}: AddCSVButtonProps) {
-    useEffect(() => {
-
-        addLocation("MSU", "case");
-    }, []);
+export default function AddCSVButton({title}: AddCSVButtonProps) {
+    const [name, setName] = useState<string>("");
+    const [amount, setAmount] = useState<string>("");
+    const [type, setType] = useState<string>("");
+    const [location, setLocation] = useState<string>("");
 
     return (
-        <div className="bg-[lightgrey] items-center rounded-lg p-5 flex flex-row gap-10 align-vertical h-min">
-            <p className="font-semibold">{name}</p>
+        <div className="bg-[lightgrey] items-center rounded-lg p-5 flex gap-10 align-vertical h-min">
+            <p className="font-semibold">{title}</p>
+            <div>
+                <p>Name</p>
+                <input type='text' value={name} onChange={setName} placeholder="Name" />
+            </div>
+            <div>
+                <p>Name</p>
+                <input type='text' value={amount} onChange={setAmount} placeholder="Name" />
+            </div>
+            <div>
+                <p>Name</p>
+                {/* <input type='text' value={name} onChange={setName} placeholder="Name" /> */}
+            </div>
+            <div>
+                <p>Name</p>
+                {/* <input type='text' value={name} onChange={setName} placeholder="Name" /> */}
+            </div>
             
-            <Button>Import CSV</Button>
+            <Button onClick={() => {
+                addLocation("MSU", "case");
+            }}>Add</Button>
         </div>
     );
 }
