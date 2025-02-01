330a3bfd1466f1a4850a36896de7f4fffa4e34f7
arnavg34 <arnavg3410@gmail.com>
2025-02-01 16:34:17 -0500

    input sidebar and base table

--- app/admin/input/page.tsx
+++ app/admin/input/page.tsx
@@ -1,10 +1,26 @@
-import AddCSVButton from "@/components/addCSVButton";
 
-export default function Home() {
+import { Payment, columns } from "./columns"
+import { DataTable } from "./data-table"
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ]
+}
+
+export default  async function Home() {
+  const data = await getData()
   return (
-    <div className="flex min-h-screen p-8 pb-20 gap-16">
-        <AddCSVButton name="In / Out" />    
-        <AddCSVButton name="Flow" />    
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
     </div>
+    
   );
 }
--- components/ui/app-sidebar.tsx
+++ components/ui/app-sidebar.tsx
@@ -1,5 +1,6 @@
-import { Calendar, Home, Inbox, Search, Settings, LogIn } from "lucide-react"
-
+"use client"
+import { Home, Settings, LogIn, LayoutDashboard, FileInput, Database } from "lucide-react"
+import { useRouter } from 'next/navigation'
 import {
   Sidebar,
   SidebarContent,
@@ -10,6 +11,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import { Button } from "./button";
 
 // Menu items.
 const items = [
@@ -19,54 +21,76 @@ const items = [
     icon: Home,
   },
   {
-    title: "Inbox",
+    title: "Dashboard",
     url: "#",
-    icon: Inbox,
+    icon: LayoutDashboard,
   },
   {
-    title: "Calendar",
+    title: "Input",
     url: "#",
-    icon: Calendar,
+    icon: FileInput,
   },
+//   {
+//     title: "Login",
+//     url: "/admin/logIn",
+//     icon: LogIn,
+//   },
   {
-    title: "Search",
+    title: "Data",
     url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
-  {
-    title: "Login",
-    url: "/admin/logIn",
-    icon: LogIn,
-  },
-]
+    icon: Database,
+  }, 
+];
+const settings = [
+    {
+        title: "Settings",
+        url: "#",
+        icon: Settings,
+      },
+];
 
 export function AppSidebar() {
+    const router = useRouter()
   return (
     <Sidebar>
-      <SidebarContent>
+      <SidebarContent className="m-2">
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupLabel>
+            <LogIn></LogIn>
+            <div className="ml-2 text-lg">
+                REFRESH FOOD
+            </div>
+          </SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
+                <SidebarMenuItem key={item.title} className="mt-1">
                   <SidebarMenuButton asChild>
                     <a href={item.url}>
                       <item.icon />
-                      <span>{item.title}</span>
+                      <span className="text-lg">{item.title}</span>
                     </a>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
+              <hr></hr>
+                {settings.map((setting) => (
+                  <SidebarMenuItem key={setting.title} className="">
+                    <SidebarMenuButton asChild>
+                      <a href={setting.url}>
+                        <setting.icon />
+                        <span className="text-lg">{setting.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}              
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
+        
       </SidebarContent>
+
+      <Button variant = "destructive" onClick={() => router.push('/logIn')} className="align-bottom h-12 px-4 m-1 text-lg ">Sign Out</Button>
     </Sidebar>
   )
 }
